800b44c227d89328e75657639a958d561695e174
rosende95 <jorgelrizos@users.noreply.github.com>
2016-10-29 14:03:12 +0200

    Update Shirt.java

--- src/Shirt.java
+++ src/Shirt.java
@@ -1,8 +1,16 @@
 
 public class Shirt {
-	int talla;
+	private int talla;
 	
 	public Shirt(int t) {
-		//...
+		this.talla = t;
+	}
+	
+	public int getTalla() {
+		return talla;
+	}
+	
+	public setTalla(int t) {
+		this.talla = talla;
 	}
 }
